5707857f3583bd012f3cc3fd957dce55a4a74fa1
Mojak01 <Mojtaba.akbari2001@hotmail.com>
2026-02-11 22:33:40 +0100

    Add basic Mongodbcontext

---
+++ Labb3/Data/MongodbContext.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Driver;
+using Labb3.Models;
+
+namespace Labb3.Data
+{
+    public class MongoDbContext
+    {
+        IMongoDatabase _db;
+
+        public MongoDbContext()
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            _db = client.GetDatabase("QuizDb");
+        }
+
+        public IMongoCollection<QuestionPack> QuestionPacks =>
+            _db.GetCollection<QuestionPack>("QuestionPacks");
+    }
+}
